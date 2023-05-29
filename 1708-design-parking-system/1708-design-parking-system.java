033df67e067d96cd6797613737e5d34febaf9ac6
Rohan <86585260+RohanSharma4792@users.noreply.github.com>
2023-05-29 10:43:41 +0530

    Time: 9 ms (56.91%), Space: 44.2 MB (9.74%) - LeetHub

---
+++ 1708-design-parking-system/1708-design-parking-system.java
@@ -0,0 +1,26 @@
+class ParkingSystem {
+    private int bigSlots;
+    private int mediumSlots;
+    private int smallSlots;
+    public ParkingSystem(int big, int medium, int small) {
+        bigSlots = big;
+        mediumSlots = medium;
+        smallSlots = small;  
+    }
+    
+    public boolean addCar(int carType) {
+         if (carType == 1 && bigSlots > 0) {
+            bigSlots--;
+            return true;
+        } else if (carType == 2 && mediumSlots > 0) {
+            mediumSlots--;
+            return true;
+        } else if (carType == 3 && smallSlots > 0) {
+            smallSlots--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
