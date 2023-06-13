dfdb1561d60ab91dd38a7f1e1ea5730520cf8d06
Rohan <86585260+RohanSharma4792@users.noreply.github.com>
2023-06-13 06:19:08 -0700

    Time: 104 ms (20.79%), Space: 48.6 MB (80.58%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -2,7 +2,7 @@ class Solution {
     public int equalPairs(int[][] grid) {
         int res = 0;
         for(int i = 0 ; i < grid.length ; i++){
-            for(int j = 0 ; j < grid[0].length ; j++){
+            for(int j = 0 ; j < grid[i].length ; j++){
                 boolean same = true;
                 for(int k = 0 ; k < grid.length ; k++){
                     if(grid[i][k]!=grid[k][j]){
