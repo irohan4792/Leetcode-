81155505dbe3d846601b9e220bc5ab97abaf0bfe
Rohan <86585260+RohanSharma4792@users.noreply.github.com>
2023-07-29 22:34:55 -0700

    Time: 0 ms (100%), Space: 39.9 MB (77.62%) - LeetHub

---
+++ 1115-valid-boomerang/1115-valid-boomerang.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        int x1=points[0][0];
+        int y1=points[0][1];
+        int x2=points[1][0];
+        int y2=points[1][1];
+        int x3=points[2][0];
+        int y3=points[2][1];
+        int res=x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2);
+        return res!=0;
+    }
+}
