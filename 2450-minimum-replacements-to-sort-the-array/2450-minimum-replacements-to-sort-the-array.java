23ef6af58d1bf5e86330155abfbe53bb3048319d
Rohan <86585260+RohanSharma4792@users.noreply.github.com>
2023-08-30 09:56:40 -0700

    Time: 5 ms (62.5%), Space: 54.4 MB (84.38%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
@@ -12,24 +12,21 @@ class Solution {
         long ans = 0;
         boolean flag = checktype(nums);
         int n=nums.length;
-             int last = nums[n - 1];  // Initialize 'last' with the last element
+             int last = nums[n - 1];  
         if(flag) return 0;
         else{
-        // long ans = 0;  // Initialize the total operations count
-
-        // Traverse the array in reverse order
         for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] > last) {  // If the current element needs replacement
-                int t = nums[i] / last;  // Calculate how many times the element needs to be divided
+            if (nums[i] > last) {  
+                int t = nums[i] / last;  
                 if (nums[i] % last != 0) {
-                    t++;  // If there's a remainder, increment 't'
+                    t++;
                 }
-                last = nums[i] / t;  // Update 'last' for the next comparison
-                ans += t - 1;  // Add (t - 1) to 'ans' for the number of operations
+                last = nums[i] / t;  
+                ans += t - 1;  
             } else {
-                last = nums[i];  // Update 'last' without replacement
+                last = nums[i];  
+                }
             }
-        }
         }
         return ans; 
     }
