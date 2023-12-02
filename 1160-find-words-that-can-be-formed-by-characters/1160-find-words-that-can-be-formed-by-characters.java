f5f206784737d80d7f9983dc818e0331a5c81447
Rohan <86585260+RohanSharma4792@users.noreply.github.com>
2023-12-02 21:31:13 +0530

    Time: 7 ms (71.48%), Space: 44.9 MB (8.02%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int countCharacters(String[] words, String chars) {
+    int ans = 0;
+    int[] count = new int[26];
+    for (char c : chars.toCharArray())
+      ++count[c - 'a'];
+    for (String word : words) {
+      int[] tempCount = count.clone();
+      for (final char c : word.toCharArray())
+        if (--tempCount[c - 'a'] < 0) {
+          ans -= word.length();
+          break;
+        }
+      ans += word.length();
+    }
+    return ans;
+  }
+}
